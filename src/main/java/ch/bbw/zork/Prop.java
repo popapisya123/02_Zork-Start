ad64872169be9f63a70f1e72e693f2a85e8e3164
Martin_BBW <martin.jaskelevic@lernende.bbw.ch>
2023-01-19 08:38:22 +0100

    back option

---
+++ src/main/java/ch/bbw/zork/Prop.java
@@ -0,0 +1,12 @@
+package ch.bbw.zork;
+
+public class Prop {
+
+    private String type;
+    private String name;
+
+    public Prop(String type, String name){
+        this.type = type;
+        this.name = name;
+    }
+}
