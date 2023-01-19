b6debfc2d3be07fd26a8dbd8c58f00db307ddf06
Martin_BBW <martin.jaskelevic@lernende.bbw.ch>
2023-01-19 10:23:23 +0100

    Room search

--- src/main/java/ch/bbw/zork/Item.java
+++ src/main/java/ch/bbw/zork/Item.java
@@ -11,4 +11,28 @@ public class Item {
         this.name = name;
         this.description = description;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
